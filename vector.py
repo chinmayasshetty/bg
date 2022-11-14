c1e98df39cfac0c3255349571f5d20572a9b3144
chinmaya <chinmayacs26@gmail.com>
2022-11-14 14:57:00 +0530

    change1

---
+++ vector.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 14:51:50 2022
+
+@author: cse12
+"""
+
+import numpy as np
+row_vec=np.array([[4,-1,2]])
+print("A row vector\n",row_vec)
+print("Shape of vector=",row_vec.shape)
